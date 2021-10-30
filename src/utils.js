f87f75e695af385c7d81e97deba4e9e4194ae4f6
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2021-10-29 21:54:47 -0500

    no useless escape

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 function getVideoIdFromURL(url) {
   const VID_REGEX = new RegExp( 
-/(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/);
+/(?:youtube(?:-nocookie)?\.com\/(?:[^/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/);
   return url.match(VID_REGEX)[1];
 }
 
